b8e6c4ce299fbf9c5a4d45349f442ea7be44ff9c
Jeffrey Wong <jwong@dayspringpartners.com>
2023-05-18 14:53:36 -0700

    trim dependencies to the bare minimum

--- Tests/TestKernel.php
+++ Tests/TestKernel.php
@@ -14,20 +14,6 @@ class TestKernel extends Kernel
     public function registerBundles()
     {
         return array(
-            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new \Symfony\Bundle\TwigBundle\TwigBundle(),
-//            new \Symfony\Bundle\MonologBundle\MonologBundle(),
-//            new \Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-//            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-//            new \Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-//            new \Symfony\Bundle\AsseticBundle\AsseticBundle(),
-//            new \Propel\Bundle\PropelBundle\PropelBundle(),
-//            new \Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
-//            new \Dayspring\LoginBundle\DayspringLoginBundle(),
-
-//            new JMS\AopBundle\JMSAopBundle(),
-//            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
         );
     }
     public function registerContainerConfiguration(LoaderInterface $loader)
